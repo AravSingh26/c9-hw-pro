8d092182c1ec870b0750abaf88e31855ead0a4e0
AravSingh26 <86350862+AravSingh26@users.noreply.github.com>
2021-06-26 15:00:29 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,38 @@
+var btn_red;
+var btn_green;
+
+var r = 0;
+var g = 0;
+var b = 0;
+
+function setup() {
+  createCanvas(400, 400);  
+  btn_red=createButton("Red")
+  btn_red.position(100,100)
+  btn_red.mousePressed(redbg)
+
+  btn_green=createButton("Green")
+  btn_green.position(300,100)
+  btn_green.mousePressed(greenbg)
+
+}
+
+function draw() {
+  background(r,g,b);
+}
+
+function redbg(){
+
+  r=255;
+  g=0;
+  b=0;
+
+}
+
+function greenbg(){
+
+  g=255
+  b=0
+  r=0
+
+}
